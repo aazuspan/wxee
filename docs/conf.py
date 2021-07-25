3794510ca899c31d6711f14568be615acee49b75
Aaron Zuspan <aa.zuspan@gmail.com>
2021-07-24 19:27:47 -0700

    Docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,8 @@
 import os
 import sys
 
+from eexarray import __version__
+
 sys.path.insert(0, os.path.abspath(".."))
 
 
@@ -21,8 +23,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "eexarray"
 copyright = "2021, Aaron Zuspan"
 author = "Aaron Zuspan"
-
-version = "0.0.1"
+version = __version__
 
 # -- General configuration ---------------------------------------------------
 

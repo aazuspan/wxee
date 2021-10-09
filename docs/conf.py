d3d79c18b32dff882ccbbad17ea4e6dfaa1f8d69
Aaron Zuspan <aa.zuspan@gmail.com>
2021-10-08 22:38:53 -0700

    Bumpversion 0.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "wxee"
 copyright = "2021, Aaron Zuspan"
 author = "Aaron Zuspan"
-version = "0.1.0"
+version = "0.1.1"
 
 # -- General configuration ---------------------------------------------------
 
--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup  # type: ignore
 
-version = "0.1.0"
+version = "0.1.1"
 
 with open("README.rst") as readme_file:
     readme = readme_file.read()

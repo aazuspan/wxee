a4637d01d5b97998f961ba79a6725c77c339301d
Aaron Zuspan <aa.zuspan@gmail.com>
2021-10-17 22:53:01 -0700

    Bumpversion 0.2.0

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath(".."))
 project = "wxee"
 copyright = "2021, Aaron Zuspan"
 author = "Aaron Zuspan"
-version = "0.1.1"
+version = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 

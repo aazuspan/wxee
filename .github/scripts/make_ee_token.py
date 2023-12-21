70407690a1014ace2458e821b5324360e5cf6881
Aaron Zuspan <aa.zuspan@gmail.com>
2023-12-21 14:36:27 -0800

    Ignore docs

--- .github/scripts/make_ee_token.py
+++
@@ -1,9 +0,0 @@
-import os
-
-credentials = f'{{"refresh_token": "{os.environ["EE_TOKEN"]}"}}'
-
-credential_dir = os.path.expanduser("~/.config/earthengine/")
-os.makedirs(credential_dir, exist_ok=True)
-
-with open(os.path.join(credential_dir, "credentials"), "w") as dst:
-    dst.write(credentials)

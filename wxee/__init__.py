33b79185953e9fc64ad5508f66137992620afec4
Aaron Zuspan <aa.zuspan@gmail.com>
2025-08-22 19:22:30 -0700

    Bump 0.4.2 -> 0.5.0

--- wxee/__init__.py
+++ wxee/__init__.py
@@ -4,6 +4,6 @@ from wxee.image import Image
 from wxee.time_series import TimeSeries
 from wxee.utils import Initialize
 
-__version__ = "0.4.2"
+__version__ = "0.5.0"
 
 __all__ = ["xarray", "Image", "ImageCollection", "TimeSeries", "Initialize"]
